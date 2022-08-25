b4e41e890c9bbd4ff49d16b7f669184ccff7367a
siykt <cnsiykt@163.com>
2022-08-25 23:01:59 +0800

    feat: bot resolve

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,7 +1,8 @@
 import { NonEmptyArray } from 'type-graphql';
+import { BotResolver } from './bot/bot.resolver';
 import { FileResolver } from './file/file.resolver';
 import { UserResolver } from './user/user.resolver';
 
-const resolvers: NonEmptyArray<Function> = [UserResolver, FileResolver];
+const resolvers: NonEmptyArray<Function> = [UserResolver, FileResolver, BotResolver];
 
 export default resolvers;
