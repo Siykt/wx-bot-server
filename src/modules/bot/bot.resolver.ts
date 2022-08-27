197a5bd8df672a2681653a824e8ca59f80bd379f
siykt <cnsiykt@163.com>
2022-08-27 23:31:07 +0800

    feat: 使用关联

--- src/modules/bot/bot.resolver.ts
+++ src/modules/bot/bot.resolver.ts
@@ -1,28 +1,39 @@
-import { Arg, Mutation, Query } from 'type-graphql';
+import { Arg, FieldResolver, Mutation, Query, Resolver, Root } from 'type-graphql';
 import { Service } from 'typedi';
 import { BotContactInfo, BotModel, BotRoomInfo } from '../../models/bot';
 import { BotService } from './bot.service';
 
 @Service()
+@Resolver(BotModel)
 export class BotResolver {
   constructor(private botService: BotService) {}
 
   @Mutation(() => BotModel, { description: '创建机器人' })
-  createBot() {
-    return this.botService.createBot();
+  createBot(@Arg('botId') botId?: string) {
+    return this.botService.createBot(botId);
   }
 
-  @Query(() => [BotContactInfo], { description: '机器人的联系人信息' })
-  async botContacts(@Arg('id') id: string) {
+  @Query(() => BotModel, { description: '获取机器人', nullable: true })
+  async bot(@Arg('id') id: string) {
     const bot = this.botService.getBot(id);
+    if (!bot) return null;
+    return {
+      id: bot.id,
+      scanQrcode: await bot.asyncGetScanQrcode(),
+    };
+  }
+
+  @FieldResolver(() => [BotContactInfo], { description: '机器人的联系人信息' })
+  async botContacts(@Root() root: BotModel): Promise<BotContactInfo[]> {
+    const bot = this.botService.getBot(root.id);
     if (!bot) return [];
     await bot.waitingReady();
     return bot.ctx.contactInfoList;
   }
 
-  @Query(() => [BotRoomInfo], { description: '机器人的所有群信息' })
-  async botRooms(@Arg('id') id: string) {
-    const bot = this.botService.getBot(id);
+  @FieldResolver(() => [BotRoomInfo], { description: '机器人的所有群信息' })
+  async botRooms(@Root() root: BotModel): Promise<BotRoomInfo[]> {
+    const bot = this.botService.getBot(root.id);
     if (!bot) return [];
     await bot.waitingReady();
     return bot.ctx.roomInfoList;
