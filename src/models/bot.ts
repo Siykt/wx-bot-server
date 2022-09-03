efac4ed6d8cb03b3ba36fd4c7fd3b9d9c3fcc047
siykt <cnsiykt@163.com>
2022-09-03 23:47:31 +0800

    feat: 调整createBot逻辑

--- src/models/bot.ts
+++ src/models/bot.ts
@@ -1,16 +1,37 @@
 import { BotContactGender, BotContactType } from '@prisma/client';
-import { Field, ObjectType } from 'type-graphql';
+import { Field, ObjectType, registerEnumType } from 'type-graphql';
+
+registerEnumType(BotContactGender, {
+  name: 'BotContactGender',
+  description: '性别',
+  valuesConfig: {
+    Unknown: { description: '未知' },
+    Male: { description: '男' },
+    Female: { description: '女' },
+  },
+});
+
+registerEnumType(BotContactType, {
+  name: 'BotContactType',
+  description: '账号类型',
+  valuesConfig: {
+    Unknown: { description: '未知' },
+    Individual: { description: '个人' },
+    Official: { description: '公众号' },
+    Corporation: { description: '团体' },
+  },
+});
 
 @ObjectType({ description: '机器人模型' })
 export class Bot {
   @Field({ description: '机器人实例id' })
   id!: string;
 
-  @Field({ description: '机器人账号名称' })
-  name!: string;
+  @Field(() => String, { description: '机器人账号名称', nullable: true })
+  name?: string | null;
 
-  @Field({ description: '登录的二维码', nullable: true })
-  scanQrcode?: string;
+  @Field(() => String, { description: '登录的二维码', nullable: true })
+  scanQrcode?: string | null;
 
   @Field({ description: '创建时间' })
   createdAt!: Date;
@@ -30,7 +51,7 @@ export class BotContact {
   @Field({ description: '联系人名称' })
   name!: string;
 
-  @Field({ description: '性别' })
+  @Field(() => BotContactGender, { description: '性别' })
   gender!: BotContactGender;
 
   @Field(() => String, { description: '别名(备注)', nullable: true })
@@ -39,7 +60,7 @@ export class BotContact {
   @Field(() => String, { description: '地址', nullable: true })
   address?: string | null;
 
-  @Field({ description: '个人/公众号' })
+  @Field(() => BotContactType, { description: '个人/公众号' })
   type!: BotContactType;
 
   @Field({ description: '创建时间' })
