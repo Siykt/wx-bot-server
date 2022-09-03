d27b3d387f006d3fe38d78ca643bc74242e1947a
siykt <cnsiykt@163.com>
2022-09-03 23:47:47 +0800

    feat: 自动回复与定时任务

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,8 +1,9 @@
 import { NonEmptyArray } from 'type-graphql';
+import { AutoReplyResolver } from './autoReply/autoReply.resolver';
 import { BotResolver } from './bot/bot.resolver';
 import { FileResolver } from './file/file.resolver';
 import { UserResolver } from './user/user.resolver';
 
-const resolvers: NonEmptyArray<Function> = [UserResolver, FileResolver, BotResolver];
+const resolvers: NonEmptyArray<Function> = [UserResolver, FileResolver, BotResolver, AutoReplyResolver];
 
 export default resolvers;
