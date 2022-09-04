551bfe951ae131cc54cd6dbc015c5790af6cd2a8
siykt <cnsiykt@163.com>
2022-09-04 23:51:46 +0800

    feat: 调整id字段

--- src/modules/bot/bot.service.ts
+++ src/modules/bot/bot.service.ts
@@ -1,4 +1,3 @@
-import { nanoid } from 'nanoid';
 import { Service } from 'typedi';
 import prisma from '../../common/prisma';
 import { BotContact, BotRoom } from '../../models/bot';
@@ -7,7 +6,7 @@ import Bot, { BotStatus } from './bot.class';
 @Service()
 export class BotService {
   async createBotByLocal(id: string, name?: string) {
-    const bot = new Bot(name, false);
+    const bot = new Bot(name, true);
     await bot.start();
     Bot.setBot(id, bot);
     return bot;
@@ -17,35 +16,31 @@ export class BotService {
     return Bot.getBot(id);
   }
 
-  async getBotContacts(id: string): Promise<BotContact[]> {
-    const bot = this.getBotByLocal(id);
+  async getBotContacts(botId: string): Promise<BotContact[]> {
+    const bot = this.getBotByLocal(botId);
     if (!bot || bot.botStatus !== BotStatus.Ready) return [];
     const contactList = await bot.getAllContact();
-    return Promise.all(
-      contactList.map(async (contact) => prisma.botContact.create({ data: { ...contact, id: nanoid(), botId: id } }))
-    );
+    return Promise.all(contactList.map(async (contact) => prisma.botContact.create({ data: { ...contact, botId } })));
   }
 
-  async getBotRooms(id: string): Promise<BotRoom[]> {
-    const bot = this.getBotByLocal(id);
+  async getBotRooms(botId: string): Promise<BotRoom[]> {
+    const bot = this.getBotByLocal(botId);
     if (!bot || bot.botStatus !== BotStatus.Ready) return [];
     const roomList = await bot.getAllRoom();
     const res: BotRoom[] = [];
-    for (const { topic, announce, alias, member } of roomList) {
+    for (const { id: roomId, topic, announce, alias, member } of roomList) {
       res.push(
         await prisma.botRoom.create({
           data: {
-            id: nanoid(),
+            id: roomId,
             topic,
             announce,
             alias,
-            botId: id,
+            botId,
           },
         })
       );
-      await Promise.all(
-        member.map(async (contact) => prisma.botContact.create({ data: { ...contact, id: nanoid(), botId: id } }))
-      );
+      await Promise.all(member.map(async (contact) => prisma.botContact.create({ data: { ...contact, botId } })));
     }
     return res;
   }
